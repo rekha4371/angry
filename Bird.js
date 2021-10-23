5b8391d75404b6d97c134f993f56688808d5c676
rekha4371 <76061600+rekha4371@users.noreply.github.com>
2021-10-23 14:57:58 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,12 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,100,100);
+    this.image = loadImage("sprites/bluebird.png");
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    super.display();
+  }
+}
